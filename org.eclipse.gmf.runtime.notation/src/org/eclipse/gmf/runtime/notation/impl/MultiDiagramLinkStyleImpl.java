4c56d633defa01a4fb0cc05f549585b799bb7651
ahunter <ahunter>
2007-06-18 16:42:50 +0000

    gmf_head ahunter 070617 Update Copyright for files changed in 2007 for GMF 2.0

--- org.eclipse.gmf.runtime.notation/src/org/eclipse/gmf/runtime/notation/impl/MultiDiagramLinkStyleImpl.java
+++ org.eclipse.gmf.runtime.notation/src/org/eclipse/gmf/runtime/notation/impl/MultiDiagramLinkStyleImpl.java
@@ -1,9 +1,13 @@
-/**
- * <copyright>
- * </copyright>
+/******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * $Id: MultiDiagramLinkStyleImpl.java,v 1.2 2007/05/22 19:10:13 aboyko Exp $
- */
+ * Contributors:
+ *    IBM Corporation - initial API and implementation 
+ ****************************************************************************/
 package org.eclipse.gmf.runtime.notation.impl;
 
 import java.util.Collection;
--- org.eclipse.gmf.runtime.notation/src/org/eclipse/gmf/runtime/notation/impl/PropertyValueImpl.java
+++ org.eclipse.gmf.runtime.notation/src/org/eclipse/gmf/runtime/notation/impl/PropertyValueImpl.java
@@ -1,9 +1,13 @@
-/**
- * <copyright>
- * </copyright>
+/******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * $Id: PropertyValueImpl.java,v 1.1 2007/05/17 16:26:50 aboyko Exp $
- */
+ * Contributors:
+ *    IBM Corporation - initial API and implementation 
+ ****************************************************************************/
 package org.eclipse.gmf.runtime.notation.impl;
 
 import org.eclipse.emf.common.notify.Notification;
